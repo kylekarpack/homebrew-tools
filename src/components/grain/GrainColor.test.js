b2bd16d8ba073d2980e867c817fcc40b2f7afdd8
Kyle Karpack <kylekarpack@gmail.com>
2020-05-25 06:49:17 -0700

    Add a few more tests

--- src/components/grain/GrainColor.test.js
+++ src/components/grain/GrainColor.test.js
@@ -1,6 +1,6 @@
 import { shallow } from "enzyme";
 import React from "react";
-import GrainColor, { doubleToHex, srmToHex, getStyle } from "./GrainColor";
+import GrainColor, { srmToHex, getStyle } from "./GrainColor";
 
 describe("Grain color component", () => {
 	it("renders without crashing", () => {
@@ -11,7 +11,15 @@ describe("Grain color component", () => {
 		expect(getStyle(1)).toHaveProperty("background");
 	});
 
-	it("srm converted to hex", () => {
+	it("converts 0 srm to hex", () => {
+		expect(srmToHex(0)).toEqual("#f0efb5");
+	});
+
+	it("converts 0.5 srm to hex", () => {
+		expect(srmToHex(0.5)).toEqual("#f0efb5");
+	});
+
+	it("converts 3 srm to hex", () => {
 		expect(srmToHex(3)).toEqual("#e1c336");
 	});
 });
