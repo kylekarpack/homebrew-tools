d185871e20fec84a7952fc911473e92961450b2d
Kyle Karpack <kylekarpack@gmail.com>
2020-08-20 22:42:20 -0700

    Add SonarQube

--- src/components/Menu.test.jsx
+++ src/components/Menu.test.jsx
@@ -1,20 +1,28 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
-import { HomebrewMenu } from "./Menu";
+import { MemoryRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { HomebrewMenu } from "./Menu";
+import { act } from "react-dom/test-utils";
+
 
 describe("Menu component", () => {
-	it("renders without crashing", () => {
+	it("renders without crashing", async () => {
 		// ToDo: move to global mocks
 		window.chooseFileSystemEntries = jest.fn().mockImplementation(async () => {
 			return {
 				getEntries: () => [],
 			};
 		});
-		shallow(
+		const component = mount(
 			<RecoilRoot>
-				<HomebrewMenu />
+				<MemoryRouter>
+					<HomebrewMenu />
+				</MemoryRouter>
 			</RecoilRoot>
 		);
+		await act(async () => { 
+			component.find("#fs").simulate("click");
+		});
 	});
 });
