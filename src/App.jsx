66414a8c94a63a561e8347c8284d32226d7e09ed
Kyle Karpack <kylekarpack@gmail.com>
2020-10-11 12:53:39 -0700

    Basic ANT added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,69 @@
 import "@fortawesome/fontawesome-free/css/all.css";
-import "bulma/css/bulma.css";
+import { Breadcrumb, Layout, Menu } from "antd";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import "./App.css";
 import { HomebrewMenu } from "./components/Menu";
+import logo from "./img/logo.svg";
 import { Fermentables, Hops, Recipe, RecipeList, Yeasts } from "./pages";
+
+const { Header, Content, Sider } = Layout;
 
 export default function App() {
 	return (
 		<RecoilRoot>
 			<Router>
-				<div className="container">
-					<div className="columns">
-						<div className="column is-one-fifth">
-							<HomebrewMenu></HomebrewMenu>
+				<Layout>
+					<Header className="header">
+						<div className="logo" style={{ float: "left" }}>
+							<Link to="/">
+								<img
+									alt="Homebrew Logo"
+									src={logo}
+									style={{
+										display: "block",
+										maxWidth: "100%",
+										maxHeight: "32px",
+										textAlign: "left",
+									}}
+								/>
+							</Link>
 						</div>
-						<div className="column">
-							<Switch>
-								<Route path="/recipes/:id" component={Recipe} />
-								<Route path="/recipes" component={RecipeList} />
-								<Route path="/fermentables" component={Fermentables} />
-								<Route path="/yeasts" component={Yeasts} />
-								<Route path="/hops" component={Hops} />
-							</Switch>
-						</div>
-					</div>
-				</div>
+						<Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+							<Menu.Item key="1">nav 1</Menu.Item>
+							<Menu.Item key="2">nav 2</Menu.Item>
+							<Menu.Item key="3">nav 3</Menu.Item>
+						</Menu>
+					</Header>
+					<Layout>
+						<Sider width={200} className="site-layout-background">
+							<HomebrewMenu />
+						</Sider>
+						<Layout style={{ padding: "0 24px 24px" }}>
+							<Breadcrumb style={{ margin: "16px 0" }}>
+								<Breadcrumb.Item>Home</Breadcrumb.Item>
+								<Breadcrumb.Item>List</Breadcrumb.Item>
+								<Breadcrumb.Item>App</Breadcrumb.Item>
+							</Breadcrumb>
+							<Content
+								className="site-layout-background"
+								style={{
+									padding: 24,
+									margin: 0,
+									minHeight: 280,
+								}}>
+								<Switch>
+									<Route path="/recipes/:id" component={Recipe} />
+									<Route path="/recipes" component={RecipeList} />
+									<Route path="/fermentables" component={Fermentables} />
+									<Route path="/yeasts" component={Yeasts} />
+									<Route path="/hops" component={Hops} />
+								</Switch>
+							</Content>
+						</Layout>
+					</Layout>
+				</Layout>
 			</Router>
 		</RecoilRoot>
 	);
